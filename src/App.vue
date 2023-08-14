60286c8d1ff1814013b7049371bef5805c3c1818
Lemon Patwari <lemonpatwari@gmail.com>
2023-08-15 00:42:33 +0600

    task 2 pie chart add

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 
 import Carousel from "./components/Carousel.vue";
+import PieChart from "./components/PieChart.vue";
 </script>
 
 <template>
   <div>
     <Carousel/>
+    <PieChart/>
   </div>
 </template>
 
